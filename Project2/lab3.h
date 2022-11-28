31a7150cbd5758e8d641078de9cc2c4facaafafd
SyzonenkoYe <yelyzaveta.syzonenko@gmail.com>
2022-11-29 00:02:28 +0200

    Init application

---
+++ Project2/lab3.h
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <fstream>
+#include <locale>
+#include <math.h>
+using namespace std;
+
+//If user enter incorrect value, for example, character, then program should generate exception.
+void checkValidInput()
+{
+    if (cin.fail())
+    {
+        throw "Incorrect input";
+    }
+}
+
+//If user enter incorrect value for from, step or n; for example, the value of n is less than 3, then program should generate exception.
+void checkValidParams(double from, double to, double step, int n)
+{
+    if (from >= to || step <= 0 || n < 3)
+    {
+        throw "Input correct data";
+    }
+}
+
+//The function “Calculate” shows how to calculate value on definite range of expression from lab 2
+double calculate(double from, double to, double step, double& x, int n)
+{
+    if (x < 0)
+    {
+        double y = 0;
+        for (int i = 0; i <= n; i++)
+        {
+            double p = 1;
+            for (int j = i; j <= n - 1; j++)
+            {
+                p *= (pow(i, i) + j);
+            }
+            y += p;
+        }
+        return y;
+    }
+    else
+    {
+        double y = 0;
+        for (int i = 1; i <= n - 2; i++)
+        {
+            y += (i - x);
+        }
+        return y;
+    }
+}
+
+//The “Main” function
+int main()
+{
+    double from, to, step, x, y;
+    int n;
+    try
+    {
+        cout << "Enter from = ";
+        cin >> from;
+        cout << "Enter to = ";
+        cin>>to;
+        cout << "Enter step = ";
+        cin>>step;
+        cout << "Enter n = "; 
+        cin >> n;
+        checkValidInput();
+        checkValidParams(from, to, step, n);
+
+        //Write on file "result.txt" from, to, step and n
+        ofstream fout("result.txt");
+            fout << "from = " << from << endl;
+            fout << "to = " << to << endl;
+            fout << "step = " << step << endl;
+            fout << "n = " << n << endl;
+        for (x = from; x <= to; x += step)
+        {
+            cout << " x = " << x << "; " << "y = " << calculate(from, to, step, x, n) << endl;
+            //Write on file "result.txt" the result of every step  
+            fout << "x = " << x <<"; ";
+            fout << "y = " << calculate(from, to, step, x, n) << " " << endl;
+        }
+    }
+    catch (const char* ex)
+    {
+        cout << ex << endl;
+        return -1;
+    }
+    catch (...)
+    {
+        cout << "Unknown error" << endl;
+        return -2;
+    }
+
+}
+
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,115 @@
+﻿
+
+
+ 
+#include "pch.h"
+#include "CppUnitTest.h" 
+#include "C:\Users\lizas\Desktop\ВИШ\ІПЗ\ЛБ3\Project2\Project2\lab3.cpp" 
+#include <expected> 
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+
+{
+	//Unit tests for Calculate function
+	TEST_CLASS(calculate_Tests)
+    {
+
+    public:
+    TEST_METHOD(calculate_get9and2and4and3_14returned)
+    {
+           double from = 2;
+           double to = 4;
+		   double step = 3;
+		   int n = 9;
+		   int expected = 10000000;
+		   double x = from;
+		   int actual = calculate(from, to, step, x, n);
+		   Assert::AreEqual(expected, actual);
+	}
+
+	public:
+
+		TEST_METHOD(calculate_get3and8and6and9_7returned)
+
+		{
+            double from = 3;
+			double to = 8;
+			double step = 6;
+			int n = 9;
+			int expected = 300;
+			double x = from;
+			int actual = calculate(from, to, step, x, n);
+			Assert::AreEqual(expected, actual);
+		}
+
+	};
+
+
+    //Unit tests for checkValidParams function
+	TEST_CLASS(checkValidParams_Tests)
+
+	{
+
+	public:
+
+		TEST_METHOD(checkValidParams_get10_exceptionNotThrown)
+
+		{
+			double from = 4;
+			double to = 9;
+			double step = 2;
+			int n = 10;
+
+
+
+			try
+
+			{
+				checkValidParams(from, to, step, n);
+				Assert::IsTrue(true);
+			}
+
+			catch (...)
+
+			{
+				Assert::Fail();
+
+			}
+
+		}
+
+	public:
+
+		TEST_METHOD(checkValidParams_get2_exceptionThrown)
+
+		{
+
+			double from = 5;
+			double to = 3;
+			double step = 2;
+			int n = 2;
+
+            try
+
+			{
+
+				checkValidParams(from, to, step, n);
+				Assert::Fail();
+
+			}
+
+			catch (...)
+
+			{
+
+				Assert::IsTrue(true);
+
+			}
+
+		}
+
+	};
+
+}
